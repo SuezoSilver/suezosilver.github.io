4f5f940a34a26716d944d9cb2d7f25e85bd1066f
SuezoSilver <suezosilver@gmail.com>
2024-03-11 14:08:54 +0800

    Update news.js

--- news/html/news.js
+++ news/html/news.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    fetch('/test/news/html/news.json') // 修改为你的 JSON 数据源
+    fetch('/news/html/news.json') // 修改为你的 JSON 数据源
     .then(response => response.json())
     .then(data => {
         var accordion = document.getElementById('accordion');
