fead82724459e68a181f1ae53246138dc7d60cb8
SuezoSilver <suezosilver@gmail.com>
2024-03-11 15:56:59 +0800

    Update news.js

--- news/html/news.js
+++ news/html/news.js
@@ -1,63 +1,67 @@
 window.onload = function() {
-    fetch('./news.json') // 修改为你的 JSON 数据源
-    .then(response => response.json())
-    .then(data => {
-        var accordion = document.getElementById('accordion');
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', './news.json');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 &&  xhr.status === 200) {
+			var data = JSON.parse(xhr.responseText);
 
-        // 只选取前10条新闻
-        var topNews = data.slice(0, 10);
+			var accordion = document.getElementById('accordion');
 
-        // 遍历 JSON 数据中的每一条新闻
-        topNews.forEach((newsItem) => {
-            var newsContainer = document.createElement('div');
+			// 只选取前10条新闻
+			var topNews = data.slice(0, 10);
 
-            // 对 newsItem.category 进行 switch-case 判断
-            let icon;
-            switch (newsItem.category) {
-                case '綜合':
-                    icon = 'info';
-                    break;
-                case '重要':
-                    icon = 'danger';
-                    break;
-                case '維護':
-                    icon = 'warning';
-                    break;
-                case '活動':
-                    icon = 'success';
-                    break;
-                case '更新':
-                    icon = 'purple';
-                    break;
-                default:
-                    icon = 'other';
-            }
+			// 遍历 JSON 数据中的每一条新闻
+			topNews.forEach((newsItem) => {
+				var newsContainer = document.createElement('div');
 
-            // 设置新闻容器内容
-            newsContainer.innerHTML = `
-                <h3 class="accordion-header"><span class="label label-${icon} label-white middle">${newsItem.category}</span> ${newsItem.title}</h3>
-                <div>
-                    <p>
-                        ${newsItem.content}
-                    </p>
-                </div>
-            `;
+				// 对 newsItem.category 进行 switch-case 判断
+				let icon;
+				switch (newsItem.category) {
+					case '綜合':
+						icon = 'info';
+						break;
+					case '重要':
+						icon = 'danger';
+						break;
+					case '維護':
+						icon = 'warning';
+						break;
+					case '活動':
+						icon = 'success';
+						break;
+					case '更新':
+						icon = 'purple';
+						break;
+					default:
+						icon = 'other';
+				}
 
-            // 添加新闻容器到 accordion
-            accordion.appendChild(newsContainer);
-        });
+				// 设置新闻容器内容
+				newsContainer.innerHTML = `
+					<h3 class="accordion-header"><span class="label label-${icon} label-white middle">${newsItem.category}</span> ${newsItem.title}</h3>
+					<div>
+						<p>
+							${newsItem.content}
+						</p>
+					</div>
+				`;
 
-        // 更新或执行'accordion'
-        if($.isFunction($.fn.accordion)){
-            $( "#accordion" ).accordion("refresh");
-        } else {
-            $( "#accordion" ).accordion({
-                collapsible: true ,
-                heightStyle: "content",
-                animate: 250,
-                header: ".accordion-header"
-            });
-        }
-    })
-    .catch((error) => console.error('Error:', error));
+				// 添加新闻容器到 accordion
+				accordion.appendChild(newsContainer);
+			});
+
+			// 更新或执行'accordion'
+			if($.isFunction($.fn.accordion)){
+				$( "#accordion" ).accordion("refresh");
+			} else {
+				$( "#accordion" ).accordion({
+					collapsible: true ,
+					heightStyle: "content",
+					animate: 250,
+					header: ".accordion-header"
+				});
+			}
+		}
+	};
+	xhr.send();
 };
