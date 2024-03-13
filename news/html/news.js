334327fa6620c7ec47287666385a4497a7a8fb92
SuezoSilver <suezosilver@gmail.com>
2024-03-13 14:56:39 +0800

    Update news.js

--- news/html/news.js
+++ news/html/news.js
@@ -38,7 +38,7 @@ window.onload = function() {
 				
 				// 设置新闻容器内容
 				newsContainer.innerHTML = 
-					'<h3 class="accordion-header" style="letter-spacing: 0.1em;">' +
+					'<h3 class="accordion-header" style="letter-spacing: 0.05em;">' +
 					'<span class="label label-' + icon +  ' label-white middle">' + newsItem.category + '</span> ' + newsItem.title + 
 					'</h3>' +
 					'<div>' +
